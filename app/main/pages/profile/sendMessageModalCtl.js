fe82914d4735f8615c00149a656af58cd0c804ea
Ben noach <bangoman@gmail.com>
2016-07-07 18:36:23 +0300

    modal

---
+++ app/main/pages/profile/sendMessageModalCtl.js
@@ -0,0 +1,23 @@
+
+
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.pages.profile')
+        .controller('sendMessageModalCtl', sendMessageModalCtl);
+            function sendMessageModalCtl($scope, $mdDialog) {
+                var vm = this;
+
+                vm.hide = function() {
+                    $mdDialog.hide();
+                };
+                vm.cancel = function() {
+                    $mdDialog.cancel();
+                };
+                vm.answer = function(answer) {
+                    $mdDialog.hide(answer);
+                };
+            }
+})();
