06d4f6ebaa9f5d47c32230e9452c5317a5e1a251
Ben noach <bangoman@gmail.com>
2016-07-07 17:51:38 +0300

    layUotStyle

--- app/main/pages/profile/profile.controller.js
+++ app/main/pages/profile/profile.controller.js
@@ -7,7 +7,7 @@
         .controller('ProfileController', ProfileController);
 
     /** @ngInject */
-    function ProfileController(Timeline, About, PhotosVideos)
+    function ProfileController(Timeline, About, PhotosVideos, $mdDialog, $mdMedia)
     {
         var vm = this;
 
@@ -18,6 +18,50 @@
         vm.photosVideos = PhotosVideos.data;
 
         // Methods
+        /*
+        vm.showSendMessage = function(ev) {
+            var confirm = $mdDialog.prompt()
+                .title('Leave a message')
+                .textContent('Whats on your mind?')
+                .placeholder('Write a message here')
+                //.ariaLabel('Dog name')
+                .targetEvent(ev)
+                .ok('Send')
+                .cancel('Cancel');
+            $mdDialog.show(confirm).then(function(result) {
+                vm.message = result;
+                console.log('vm.message: ',vm.message);
+            }
+            //, function() {
+            //    somthing to commit if pressed 'cancel'
+            //}
+            );
+        };
+        */
+
+        vm.showSendMessage = function(ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'dialog1.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: useFullScreen
+            })
+            .then(function(answer) {
+                vm.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                vm.status = 'You cancelled the dialog.';
+            });
+            vm.$watch(function() {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function(wantsFullScreen) {
+                vm.customFullscreen = (wantsFullScreen === true);
+            });
+        };
+
+
 
         //////////
     }
